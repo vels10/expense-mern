ab5d9b98529b911e232e297b80ff35f517f5b095
Velkumaran AP <38457682+Vels10@users.noreply.github.com>
2020-06-10 20:08:47 +0530

    Add files via upload

---
+++ client/build/precache-manifest.cbfdfbe4985c49f9bd8e675742079a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b334a0dcd7b8838851d35fcb1a74cc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a803559ae817f87cd79e",
+    "url": "/static/css/main.474fc8c2.chunk.css"
+  },
+  {
+    "revision": "ef6ae821416f7fab407c",
+    "url": "/static/js/2.db7604d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db7604d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a803559ae817f87cd79e",
+    "url": "/static/js/main.dcc7bf38.chunk.js"
+  },
+  {
+    "revision": "0fb5aa10d11e0a3e8d13",
+    "url": "/static/js/runtime-main.56c06abf.js"
+  }
+]);
